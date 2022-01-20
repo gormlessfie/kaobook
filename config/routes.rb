b3b2b3d98026e35dfc373dac51acdcc84a7d3953
gormlessfie <gmlessfie@gmail.com>
2022-01-19 19:13:04 -0800

    Create user index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'users#index'
+  
   devise_for :users, controllers: { sessions: 'users/sessions', registration: 'users/registrations' }
-
-  root 'sessions#create'
+  
+  resources :users, only: [:index]
 end
