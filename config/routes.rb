529a59f3e1039a4271d11963144015d13fe921a5
gormlessfie <gmlessfie@gmail.com>
2022-01-20 15:14:21 -0800

    Add css to login and registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  
   devise_for :users, controllers: { sessions: 'users/sessions', registration: 'users/registrations' }
+
+  devise_scope :user do
+    authenticated :user do
+      root 'users#show', as: :authenticated_root
+    end
+
+    unauthenticated :user do
+      root 'users/sessions#new', as: :unauthenticated_root
+    end
+  end
   
   resources :users, only: [:index]
 end
