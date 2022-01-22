4382048d93d310f1dcf64bd26e390f06e7726e9a
gormlessfie <gmlessfie@gmail.com>
2022-01-22 13:55:15 -0800

    Create user index page, search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.order(username: :asc)
+    @result = User.where('username LIKE ?', "%#{params[:query]}%") if params[:query]
   end
 
   def show
-    # profile
+    @user = User.find(params[:id])
   end
 end
