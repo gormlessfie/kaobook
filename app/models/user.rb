e3ca28173da87acf51e0e38160044360b952fc3a
gormlessfie <gmlessfie@gmail.com>
2022-01-22 12:45:17 -0800

    Add button to users#index

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
+  validates :username, presence: true, length: { in: 3..12 }
+
   has_many :comments, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :friendships, dependent: :destroy
