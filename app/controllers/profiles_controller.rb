70a18c8ec7d562573cb84818f0be9e13827157ea
gormlessfie <gmlessfie@gmail.com>
2022-01-23 15:13:59 -0800

    show profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,5 @@
 class ProfilesController < ApplicationController
   def show
+    @profile = User.find(params[:id]).profile.includes(detail: [:age, :location, :gender, :contact_information, :name])
   end
 end
