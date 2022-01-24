8571c75fcbae39ac5951599b0989b6fe92315804
gormlessfie <gmlessfie@gmail.com>
2022-01-23 16:06:51 -0800

    Install active_storage and install imagemagicks

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,22 @@ class ProfilesController < ApplicationController
   def show
     @profile = User.find(params[:user_id]).profile
   end
+
+  def update
+    @profile = current_user.profile
+
+    if @profile.update(profile_params)
+      flash[:success] = 'Profile picture uploaded.'
+      redirect_to user_profile_path(current_user)
+    else
+      flash[:error] = 'Failed to upload profile picture.'
+      redirect_to user_profile_path(current_user)
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:avatar)
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,5 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   has_one :detail, dependent: :destroy
+  has_one_attached :avatar, dependent: :destroy
 end
