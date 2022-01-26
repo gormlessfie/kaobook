b232132a7c0f10bafe9db73e04e03ee289c56bc4
gormlessfie <gmlessfie@gmail.com>
2022-01-26 15:06:03 -0800

    Fix omniauth callback controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :create
 
   def facebook
-    @user = from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
