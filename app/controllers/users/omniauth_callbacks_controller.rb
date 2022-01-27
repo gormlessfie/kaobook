7ee8f67cf6bb1a7b5c0f7b73e545e953aabb5d26
gormlessfie <gmlessfie@gmail.com>
2022-01-26 16:11:20 -0800

    Change how omniauth redirects

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,8 +8,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in @user
-      redirect_to new_user_detail_path(@user)
-  
+      redirect_to authenticated_root_path, event: :authentication
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra)
