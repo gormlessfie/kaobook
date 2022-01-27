9df6e8111557214dc5e4b2feea27547a9d7a5330
gormlessfie <gmlessfie@gmail.com>
2022-01-26 21:07:01 -0800

    Change default host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'quiet-temple-02644.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'kevin-luong.net' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
