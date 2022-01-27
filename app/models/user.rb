f0fb219ed715c76c735d20f212db17930ea3be6c
gormlessfie <gmlessfie@gmail.com>
2022-01-27 14:08:49 -0800

    make username unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ApplicationRecord
     created_user = where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.username = "#{full_name[0]}_#{full_name[1]}"
+      user.username = "#{full_name[0]}_#{full_name[1]}_#{rand(10000..30000)}"
 
       user.skip_confirmation!
     end
